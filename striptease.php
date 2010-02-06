cb1268ff2be38699ead46492a4592ffff4608dd2
guyfisher <guyfisher@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-02-06 20:52:05 +0000

    Updated plugin header and license
    git-svn-id: https://plugins.svn.wordpress.org/striptease/branches/2.0@202681 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- striptease.php
+++ striptease.php
@@ -3,30 +3,25 @@
 /*
 
 Plugin Name: StripTease
-Description: Strip "#more" fragments from "Read More" teaser links.
 Plugin URI: http://guyfisher.com/builder/striptease/
-Author: Guy M. Fisher
+Description: Strips the "#more" fragments from the end of "Read More" teaser links.
+Author: Guy Fisher
 Author URI: http://guyfisher.com/
-Version: 1.1
+Version: 2.0
 
-Copyright (c) 2005 by Guy M. Fisher <guyfisher@yahoo.com>
+Copyright © 2010 Guy M. Fisher
 
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
 
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
-
-For a summary of your rights under the GNU General Public License,
-please visit http://creativecommons.org/licenses/GPL/2.0/
+http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
 
 */
 
