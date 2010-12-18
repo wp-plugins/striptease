92667d7336642bfb8e4483b572165aacf6277456
guyfisher <guyfisher@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-12-18 22:28:12 +0000

    Updated plugin header and inline documentation
    git-svn-id: https://plugins.svn.wordpress.org/striptease/tags/2.0@324211 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- striptease.php
+++ striptease.php
@@ -1,14 +1,15 @@
 <?php
 
 /**
- * A WordPress plugin that strips the "#more" fragments from the end of "Read More" teaser links.
+ * A WordPress plugin that strips the "#more" fragments from the end of "Read
+ * More" teaser links.
  *
  * @package StripTease
  * @link http://guyfisher.com/builder/striptease/
  * @author Guy Fisher
- * @copyright Copyright © 2010 Guy M. Fisher
- * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
  * @version 2.0
+ * @copyright Copyright © 2010 Guy M. Fisher
+ * @license http://www.gnu.org/licenses/gpl-2.0.html
  */
 
 /*
@@ -22,11 +23,16 @@ Version: 2.0
 
 Copyright © 2010 Guy M. Fisher
 
-This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.
+This program is free software; you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation; either version 2 of the License, or (at your option) any
+later version.
 
-This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+This program is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 
-http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+http://www.gnu.org/licenses/gpl-2.0.html
 
 */
 
@@ -34,7 +40,7 @@ http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
  * Strips the #more-$id fragments from the end of "Read More" teaser links.
  *
  * Filters the HTML string passed from the_content_more_link filter in the
- * get_the_content() function.
+ * get_the_content function.
  *
  * @since 2.0
  *
